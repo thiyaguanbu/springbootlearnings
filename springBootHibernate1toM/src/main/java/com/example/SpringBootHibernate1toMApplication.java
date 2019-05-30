ffb012c3db0eee15225ee413143ad2b608fba657
thiyaguanbu <thethiyagu@yahoo.com>
2019-05-30 20:18:41 +0530

    Added files

---
+++ springBootHibernate1toM/src/main/java/com/example/SpringBootHibernate1toMApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernate1toMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernate1toMApplication.class, args);
+	}
+}
